23fca79441ad99dad800d62ec458ebee5dd0ad6f
Ryan Lei <135535159+RyanL3i@users.noreply.github.com>
2026-04-26 18:22:27 -0500

    tc2: BoardConstructor_AllSquaresInitiallyEmpty passes

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -1,7 +1,14 @@
 package domain;
 
 public class Board {
+
+    private final Object[][] squares = new Object[8][8];
+
     public int getSize() {
         return 8;
+    }
+
+    public Object getSquare(int row, int col) {
+        return squares[row][col];
     }
 }
--- src/test/java/domain/BoardTest.java
+++ src/test/java/domain/BoardTest.java
@@ -3,11 +3,20 @@ package domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class BoardTest {
     @Test
     public void BoardConstructor_SizeIsEight() {
         Board board = new Board();
         assertEquals(8, board.getSize());
+    }
+
+    @Test
+    public void BoardConstructor_AllSquaresInitiallyEmpty() {
+        Board board = new Board();
+
+        assertNull(board.getSquare(0,0));
+        assertNull(board.getSquare(7,7));
     }
 }
