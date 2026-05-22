22a6f883d763237fd09d1d0158b81eeac9e94279
Sid <siddharthjaveri2027@u.northwestern.edu>
2026-05-21 20:05:23 -0500

    TC9: MovePiece throws exception for end column above bounds

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -205,6 +205,16 @@ public class GameTest {
         assertEquals(PieceColor.WHITE, game.getCurrentTurn());
     }
 
+    @Test
+    public void MovePiece_EndColumnAboveBounds_ThrowsIndexOutOfBoundsException() {
+        Game game = new Game();
+        game.initializeGame();
+
+        assertThrows(IndexOutOfBoundsException.class, () -> game.movePiece(6, 0, 5, 8));
+
+        assertEquals(PieceColor.WHITE, game.getCurrentTurn());
+    }
+
     private void assertPiece(
             Board board,
             int row,
