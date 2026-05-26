7518dc21740731f9a1eb833d0121ef884809367c
Sid <siddharthjaveri2027@u.northwestern.edu>
2026-05-26 16:27:35 -0500

    TC4: MovePiece_BlackPawnPromotesToBishop_ReturnsBishopOnFinalRank passes

--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -2373,6 +2373,33 @@ public class GameTest {
         assertEquals(PieceColor.WHITE, game.getCurrentTurn());
     }
 
+    @Test
+    public void MovePiece_BlackPawnPromotesToBishop_ReturnsBishopOnFinalRank() {
+        Game game = new Game();
+        game.initializeGame();
+
+        Board board = game.getBoard();
+
+        for (int row = 0; row < board.getSize(); row++) {
+            for (int col = 0; col < board.getSize(); col++) {
+                board.setSquare(row, col, null);
+            }
+        }
+
+        board.setSquare(7, 4, new Piece(PieceType.KING, PieceColor.WHITE));
+        board.setSquare(0, 4, new Piece(PieceType.KING, PieceColor.BLACK));
+        board.setSquare(6, 7, new Piece(PieceType.PAWN, PieceColor.BLACK));
+
+        game.movePiece(7, 4, 7, 3);
+
+        game.movePiece(6, 7, 7, 7, PieceType.BISHOP);
+
+        assertTrue(board.isEmpty(6, 7));
+        assertEquals(PieceType.BISHOP, board.getSquare(7, 7).getType());
+        assertEquals(PieceColor.BLACK, board.getSquare(7, 7).getColor());
+        assertEquals(PieceColor.WHITE, game.getCurrentTurn());
+    }
+
     private void assertPiece(
             Board board,
             int row,
